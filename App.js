925419676af62904ba4838bde4f71b5b611e1529
Irene <irene_lee@live.com.sg>
2020-12-01 17:16:39 +0800

    First change

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
+  function renderEncouragingText() {
+    if (count > 10) {
+      return "Keep Going!"
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>{count}</Text>
+      <TouchableOpacity style={styles.button} onPress={increment}>
+        <Text style={styles.buttonText}>
+          Click Me!
+        </Text>
+      </TouchableOpacity>
+      <Text style={styles.encouragingText}>{renderEncouragingText()}</Text>
+      <TouchableOpacity style={styles.button} onPress={reset}>
+        <Text style={styles.buttonText}>
+          Reset!
+        </Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +41,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F3EFF5',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    color: '#D8C99B',
+    backgroundColor: '#2D82B7',
+    fontSize: 50,
+    borderRadius: 20,
+    padding: 10,
+    
+  },
+
+  encouragingText: {
+  marginTop:50,
+    color: 'grey',
+    fontSize: 35,
+  },
+
+  button: {
+    backgroundColor: '#2D82B7',
+    width: 80,
+    height: 30,
+    borderRadius: 20,
+    margin: 30,
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    padding: 10,
+  },
+
+  buttonText: {
+    color: '#D6F8D6',
+  }
 });
+
