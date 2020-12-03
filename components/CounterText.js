bbb3f94a2d7eccc9d9cf77fd6f654dc47067830c
Irene-miao <71961907+Irene-miao@users.noreply.github.com>
2020-12-03 11:59:27 +0800

    Update CounterText.js
    Edit

--- components/CounterText.js
+++ components/CounterText.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import { Text, Stylesheet } from 'react-native';
+import React from "react";
+import { Text, StyleSheet } from "react-native";
 
-export defaulerText(props) {
-    rett function Counturn (
-    <Text style={[ 
+// Props:
+// - color
+// - fontSize
+// - marginBottom
+// - children
+
+export default function CounterText(props) {
+  console.log(props);
+  return (
+    <Text
+      style={[
         styles.counterText,
-         { color: props.color, fontSize: props.fontSize},
-    ]}>
-        {props.children}
-        </Text> };
-        }
-
-    const styles = StyleSheet.create({
-        counterText: {
-            fontWeight: 'bold',
+        {
+          color: props.color,
+          fontSize: props.fontSize,
+          marginBottom: props.marginBottom,
         },
-    });
+      ]}
+    >
+      {props.children}
+    </Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  counterText: {
+    fontWeight: "bold",
+  },
